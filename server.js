bed5d1a5296f6b0f7b34c6bdbd190556ff4d9f5c
UchuNoKen <vanguardbosses@gmail.com>
2018-06-13 19:48:38 -0400

    Added project page

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        // currentYear: new Date().getFullYear()
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request.'
